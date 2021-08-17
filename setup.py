44a71ada694bb8a34628c51c8d6b16d36dcb151b
Saran33 <36115026+Saran33@users.noreply.github.com>
2021-08-17 23:14:35 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='pwe',
     packages=find_packages(include=['pwe']),
-    version='0.1.6',
+    version='0.1.6.1',
 	author='Saran Connolly',
     description='PWE Capital security analysis and charting package.',
 	long_description=long_description,
@@ -21,10 +21,11 @@ setup(
         "Operating System :: OS Independent",
     ],
 	install_requires = [
-  'cufflinks @ git+https://github.com/Saran33/cufflynx.git@Saran33#egg=cufflinks','pandas','numpy','pandas_summary','plotly',
+  'cufflinks @ git+https://github.com/Saran33/cufflynx.git@Saran33#egg=cufflinks',
+		'cryptocmd @ git+https://github.com/guptarohit/cryptoCMD.git','pandas','numpy','pandas_summary','plotly','quandl',
 ],
-	package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+	#package_dir={"": "src"},
+    #packages=find_packages(where="pwe"),
     python_requires=">=3.7",
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
