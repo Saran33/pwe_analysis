28efff3434ddb334ffc093c11a4fe3af6bdb3eee
Saran Connolly <36115026+Saran33@users.noreply.github.com>
2021-08-26 22:27:49 +0400

    φ, get_settle_dates

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='pwe',
     packages=find_packages(include=['pwe']),
-    version='0.1.6180339887498948482045868',
+    version='0.1.61803398874989484820458683',
 	author='Saran Connolly',
     description='PWE Capital security analysis and charting package.',
 	long_description=long_description,
