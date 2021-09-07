df3bc38454cd252aa3ce24c418f350fc5d741512
Saran Connolly <36115026+Saran33@users.noreply.github.com>
2021-09-07 14:29:29 +0400

    Update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='pwe',
     packages=find_packages(include=['pwe']),
-    version='0.1.0.1.61803398874989484820458683436',
+    version='0.1.0.1.618033988749894848204586834365',
 	author='Saran Connolly',
     description='PWE Capital security analysis and charting package.',
 	long_description=long_description,
@@ -25,7 +25,7 @@ setup(
   'alpha_vantage @ git+git://github.com/Saran33/alpha_vantage.git@develop#egg=alpha_vantage',
 		'cryptocmd @ git+https://github.com/guptarohit/cryptoCMD.git',
         'datamine @ git+https://github.com/Saran33/datamine_python.git',
-        'pandas','numpy','pandas_summary','plotly','quandl','TA-Lib','ta','tqdm','pytz','re','glob',
+        'pandas','numpy','pandas_summary','plotly','quandl','TA-Lib','ta','tqdm','pytz','glob',
 ],
 	#package_dir={"": "src"},
     #packages=find_packages(where="pwe"),
