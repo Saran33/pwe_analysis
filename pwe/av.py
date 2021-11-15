b071a35690ab8f39257b9d0f092dea0a918f72cf
Saran33 <saranconnolly618@gmail.com>
2021-11-15 21:27:02 +0400

    Update

--- pwe/av.py
+++ pwe/av.py
@@ -13,16 +13,24 @@ from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.cryptocurrencies import CryptoCurrencies
 
 
-def get_av_ts(symbol, interval, start, end, rel_dir, AV_API='ALPHAVANTAGE_API_KEY'):
+def get_av_ts(symbol, endpoint, start, end, rel_dir, AV_API='ALPHAVANTAGE_API_KEY', interval=None, outputsize='compact'):
     """
     AlphaVatage API
     https://github.com/RomelTorres/alpha_vantage
 
-    interval -> intraday, intraday_extended, daily_adjusted, daily, monthly_adjusted, monthly, weekly_adjusted, weekly, """
+    endpoint -> intraday, intraday_extended, daily, daily_adjusted, weekly, weekly_adjusted, monthly, monthly_adjusted.timeseries
+    outputsize:  The size of the call, supported values are
+        'compact' and 'full; the first returns the last 100 points in the
+        data series, and 'full' returns the full-length intraday times
+        series, commonly above 1MB (default 'compact')
+    """
 
     tkr = symbol.replace('/', '_')
 
-    file = f'{rel_dir}/{tkr}_{interval}_{start}_{end}.csv'
+    if interval:
+        file = f'{rel_dir}/{tkr}_{endpoint}_{interval}_{start}_{end}.csv'
+    else:
+        file = f'{rel_dir}/{tkr}_{endpoint}_{start}_{end}.csv'
     file_path = os.path.abspath(file)
 
     print(file_path)
@@ -39,12 +47,30 @@ def get_av_ts(symbol, interval, start, end, rel_dir, AV_API='ALPHAVANTAGE_API_KE
         print("No CSV found. Downloading data from API")
         ts = TimeSeries(key=AV_API, output_format='pandas')  # retries=5
         # Get json object with the intraday data and another with  the call's metadata
-        df, meta_data = ts.get_monthly_adjusted(tkr)
+        if endpoint == 'intraday':
+            df, meta_data = ts.get_intraday(tkr, interval=interval, outputsize=outputsize)
+        elif endpoint == 'intraday_extended':
+            df, meta_data = ts.get_intraday_extended(tkr, interval=interval, outputsize=outputsize)
+        elif endpoint == 'daily':
+            df, meta_data = ts.get_daily(tkr, outputsize=outputsize)
+        elif endpoint == 'daily_adjusted':
+            df, meta_data = ts.get_daily_adjusted(tkr, outputsize=outputsize)
+        elif endpoint == 'weekly':
+            df, meta_data = ts.get_weekly(tkr)
+        elif endpoint == 'weekly_adjusted':
+            df, meta_data = ts.get_weekly_adjusted(tkr)
+        elif endpoint == 'monthly':
+            df, meta_data = ts.get_monthly(tkr)
+        elif endpoint == 'monthly_adjusted':
+            df, meta_data = ts.get_monthly_adjusted(tkr)
         del_col_num(df)
         sort_index(df)
         df.index.names = ['DateTime']
         df.name = tkr
-        f_name = f'{rel_dir}/{tkr}_{interval}_{start}_{end}.csv'
+        if interval:
+            f_name = f'{rel_dir}/{tkr}_{endpoint}_{interval}_{start}_{end}.csv'
+        else:
+            f_name = f'{rel_dir}/{tkr}_{endpoint}_{start}_{end}.csv'
         df.to_csv(f_name)
 
     return df
