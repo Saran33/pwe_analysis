5d7ed9829085d62bd25221629094b7c77ad70743
Saran33 <saranconnolly618@gmail.com>
2022-02-04 19:53:13 +0400

    Update nomics dependency

--- setup.py
+++ setup.py
@@ -2,35 +2,36 @@ from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-	
+
 setup(
     name='pwe',
     packages=find_packages(include=['pwe']),
     version='0.1.61803398874989484820458683436563811772030917980',
-	author='Saran Connolly',
+    author='Saran Connolly',
     description='PWE Capital security analysis and charting package.',
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	url="https://github.com/Saran33/pwe_analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Saran33/pwe_analysis",
     project_urls={
         "Bug Tracker": "https://github.com/Saran33/pwe_analysis/issues",
     },
-	classifiers=[
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-	install_requires = [
-  'cufflinks @ git+https://github.com/Saran33/cufflynx.git@Saran33#egg=cufflinks',
-  'alpha_vantage @ git+git://github.com/Saran33/alpha_vantage.git@develop#egg=alpha_vantage',
-		'cryptocmd @ git+https://github.com/Saran33/cryptoCMD',
+    install_requires=[
+        'cufflinks @ git+https://github.com/Saran33/cufflynx.git@Saran33#egg=cufflinks',
+        'alpha_vantage @ git+git://github.com/Saran33/alpha_vantage.git@develop#egg=alpha_vantage',
+        'nomics @ git+git://github.com/Saran33/nomics.git',
+        'cryptocmd @ git+https://github.com/Saran33/cryptoCMD',
         'datamine @ git+https://github.com/Saran33/datamine_python.git',
         'pykalman @ git+https://github.com/pykalman/pykalman.git',
-        'pandas','numpy', 'scipy', 'pandas_summary','plotly','quandl','ta','tqdm','pytz',  # 'TA-Lib',
-        'datatile',
-],
-	#package_dir={"": "src"},
-    #packages=find_packages(where="pwe"),
+        'pandas', 'numpy', 'scipy', 'pandas_summary', 'plotly', 'quandl', 'ta', 'tqdm', 'pytz',  # 'TA-Lib',
+        'datatile', 'python-decouple',
+    ],
+    #package_dir={"": "src"},
+    # packages=find_packages(where="pwe"),
     python_requires=">=3.8",
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
